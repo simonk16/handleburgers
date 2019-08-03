3b3519383c2b7aa332b62715a0f9402b319420f9
Kraus, Simon <simon.kraus@aol.com>
2019-08-03 16:55:10 -0500

    got orm methods working in controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+
+
+var burger = require("../config/orm");
+
+router.get("/api/burg", function(req, res){
+    burger.all("Burgers", function(data){
+        console.log(data);
+        res.json(data);
+    })
+})
+
+router.post("/api/burg", function(req, res){
+    burger.create(req.body.burger_name, function(response){
+        console.log(response);
+        res.status(201).json(response);
+    })
+})
+
+router.put("/api/burg/:id", function(req, res){
+    burger.update(req.params.id, function(response){
+        console.log(response);
+        res.status(200).json(response);
+    })
+})
+module.exports = router;
